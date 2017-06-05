6ebab3b36f2cca31feabeec148b6bc21cd9e8fae
barriquello <barriquello@gmail.com>
2017-06-05 10:26:16 -0300

    melhoria na portabilidade do terminal

--- brtos/modules/terminal/terminal_cfg_def.h
+++ brtos/modules/terminal/terminal_cfg_def.h
@@ -1,18 +1,30 @@
 /*
- * terminal_cfg.h
+ * terminal_cfg_def.h
  *
  *  Created on: 28/04/2016
- *      Author: Universidade Federal
+ *  Author: Carlos H. Barriquello
  */
 
-#ifndef APP_TERMINAL_CFG_H_
-#define APP_TERMINAL_CFG_H_
+#ifndef TERMINAL_CFG_DEF_H_
+#define TERMINAL_CFG_DEF_H_
+
 
 /************* TERMINAL CONFIG *************************/
+#ifndef TERM_INPUT_BUFSIZE
 #define TERM_INPUT_BUFSIZE 		32
-#define UP_KEY_CHAR				(char)-32
-#define CHARS_TO_DISCARD		1
+#endif
 
+#ifndef UP_KEY_CHAR
+#define UP_KEY_CHAR				(char)-32
+#endif
+
+#ifndef CHARS_TO_DISCARD
+#define CHARS_TO_DISCARD		1
+#endif
+
+//** Only for reference. Copy and paste this file to a local folder, and chaneg it according your platform*/
+
+#if 0
 /* Supported commands */
 /* Must be listed in alphabetical order !!!! */
 
@@ -25,15 +37,16 @@
 
 #define HELP_DESCRIPTION         1
 
-#if WINNT
-#include <stdio.h>
-#define TERM_PRINT(...) printf(__VA_ARGS__); fflush(stdout);
-#else
+#endif
+
+#ifndef TERM_PRINT
 #include "printf_lib.h"
-#define TERM_PRINT(...) printf_lib(__VA_ARGS__);
+#define CUSTOM_PRINTF    1
+#define TERM_PRINT(...)  printf_lib(__VA_ARGS__);
+#define SPRINTF(a,...)	 snprintf_lib(a,256,__VA_ARGS__);
 #endif
 
 
 /*******************************************************/
 
-#endif /* APP_TERMINAL_CFG_H_ */
+#endif /* TERMINAL_CFG_DEF_H_ */
